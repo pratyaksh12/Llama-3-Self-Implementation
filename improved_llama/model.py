5b9df049807bf782d7eb320e94d8291b3cabfee6
pratyaksh12 <69629321+pratyaksh12@users.noreply.github.com>
2025-09-24 15:15:06 +1000

    Update model.py

--- improved_llama/model.py
+++ improved_llama/model.py
@@ -1,6 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
-
 import math
 from dataclasses import dataclass
 from typing import Optional, Tuple
